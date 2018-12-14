3c2b20ee3227ff17b3e7c2efde98f86368daa4ef
Artem Yankov <yankovai3@gmail.com>
2018-12-14 08:14:49 -0800

    adding main file

---
+++ main.py
@@ -0,0 +1,18 @@
+import math
+import os
+from hyperopt import fmin, tpe, hp
+from hyperopt.mongoexp import MongoTrials
+
+def main():
+    mongo_db_host = os.environ["MONGO_DB_HOST"]
+    mongo_db_port = os.environ["MONGO_DB_PORT"]
+    experiment_name = os.environ["EXPERIMENT_NAME"]
+
+    mongo_process = "mongo://{0}:{1}/foo_db/jobs".format(mongo_db_host, mongo_db_port)
+
+    trials = MongoTrials(mongo_process, exp_key=experiment_name)
+    best = fmin(math.sin, hp.uniform('x', -2, 2), trials=trials, algo=tpe.suggest, max_evals=10)
+    print(best)
+
+if __name__ == "__main__":
+    main()
