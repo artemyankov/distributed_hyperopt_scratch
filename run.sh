57e04c96e2162b1896f41d36707aa729be5b032d
Artem Yankov <yankovai3@gmail.com>
2018-12-14 07:28:18 -0800

    run mongo in background

--- run.sh
+++ run.sh
@@ -10,7 +10,8 @@ if [[ $JOB_NAME == "ps" ]]; then
     echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
     apt-get update
     apt-get install -y mongodb-org
-    mongod --port 5000
+    mongod --fork --logpath ./mongod.log --port 5000
+   # mongod --port 5000 &
 
     echo "[+] Im a PS running Mongo"
 else
