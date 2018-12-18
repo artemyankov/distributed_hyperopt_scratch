88361cf56c3cdac444b0941a7a6e74544dbaa5a4
Artem Yankov <yankovai3@gmail.com>
2018-12-18 08:45:08 -0800

    test json output 2

--- test_json_save.py
+++ test_json_save.py
@@ -1,8 +1,11 @@
 import json
+import os
+from clusterone import get_logs_path
 
 def main():
     test_dict = {'dog': 'bernese'}
-    with open('test_json.json', 'w') as f:
+    file_path = os.path.join(get_logs_path('./logs'), 'test_json.json')
+    with open(file_path, 'w') as f:
         json.dump(json.dumps(test_dict), f)
 
     return
